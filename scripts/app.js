67e5eb406494f6c74d8aaca3502a65f3d7922d66
colesmith001 <cole.smith@sdgku.edu>
2026-06-20 11:40:49 -0700

    second commit

--- scripts/app.js
+++ scripts/app.js
@@ -28,7 +28,7 @@ function saveTask(){
         contentType: "application/json",
         success: function(created){
             console.log(created);
-            displayTask(created);
+            // displayTask(created);
         },
         error: function(err){
             console.log(err);
@@ -76,15 +76,18 @@ function loadTask(){
 }
 function displayTask(task){
     let syntax = `
-    <div class="card-color" class="task" style="border-color:${task.color}">
+    <div  class="task"id="${task.id}" style="border-color:${task.color}">
         <div class="info">
             <h4 class="task-title">${task.title}</h4>
-            <p>${task.desc}</p>
+            <p class="task-desc"  >${task.desc}</p>
         </div>
         <label class="status">${task.status}</label>
         <div class= "date-budget">
             <label>${task.date}</label>
             <label>${task.budget}</label>
+        </div>
+        <div>
+            <button class="btn-delete">🗑️</button>
         </div>
     </div>
     `;
@@ -95,9 +98,45 @@ function displayTask(task){
 function init(){
     console.log("hello world")
     $("#btnSave").click(saveTask);
+// old way
+    // $(".btn-delete").click(deleteTask);
+
+    // new way event delegation
+    // on click inside of .list, if the target is btn
+    $(".list").on("click",".btn-delete",deleteTask);
+
+
     loadTask();
     
 }
+
+function deleteTask(){
+    // context for specific button
+    let btn = $(this);
+
+    // 2. find the parent 
+    let taskElement = btn.parents(".task");
+
+    // 3.extraction get ID that we save in the html
+    let id = taskElement.attr("id");
+
+    console.log("The requesting id is: ", id)
+
+    // 4. server connection
+    $.ajax(
+        {
+            type:"delete",
+            url: API + "/" + id,
+            success: function(){
+                taskElement.fadeOut(500,function(){
+                    $(this).remove();
+                });
+            },error: function(err){
+                    console.log("error");
+            }
+        }
+    )
+};
 
 
 
